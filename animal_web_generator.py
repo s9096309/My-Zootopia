30a2df7e3220fec91ff7dc925233d2c57387f10e
s9096309 <k-hoff-mann@web.de>
2025-02-20 16:12:48 +0100

    final commit

--- animal_web_generator.py
+++ animal_web_generator.py
@@ -5,27 +5,33 @@ def load_data(file_path):
     with open(file_path, "r") as handle:
         return json.load(handle)
 
+def serialize_animal(animal_obj):
+    """Serializes a single animal object into HTML."""
+    output = '<li class="cards__item">\n'
+    if "name" in animal_obj:
+        output += f'<div class="card__title">{animal_obj["name"]}</div>\n'
+    if "characteristics" in animal_obj and "diet" in animal_obj["characteristics"]:
+        output += f'<p class="card__text"><strong>Diet:</strong> {animal_obj["characteristics"]["diet"]}<br/>\n'
+    if "locations" in animal_obj and animal_obj["locations"]:
+        output += f'<strong>Location:</strong> {animal_obj["locations"][0]}<br/>\n'
+    if "characteristics" in animal_obj and "type" in animal_obj["characteristics"]:
+        output += f'<strong>Type:</strong> {animal_obj["characteristics"]["type"]}<br/>\n'
+    output += '</p>\n' # Close the <p> tag
+    output += '</li>\n'
+    return output
+
 def generate_animal_html(data):
-    """Generates HTML for animal information."""
+    """Generates HTML for all animal information."""
     output = ""
     for animal in data:
-        output += '<li class="cards__item">\n'  # Start of list item
-        if "name" in animal:
-            output += f"Name: {animal['name']}<br/>\n"
-        if "characteristics" in animal and "diet" in animal["characteristics"]:
-            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
-        if "locations" in animal and animal["locations"]:
-            output += f"Location: {animal['locations'][0]}<br/>\n"
-        if "characteristics" in animal and "type" in animal["characteristics"]:
-            output += f"Type: {animal['characteristics']['type']}<br/>\n"
-        output += '</li>\n'  # End of list item
+        output += serialize_animal(animal)  # Use the new function
     return output
 
 def create_animals_html(input_json, template_file, output_file):
     """Creates the final HTML file."""
     try:
         animals_data = load_data(input_json)
-        animal_html = generate_animal_html(animals_data)  # Use the new function
+        animal_html = generate_animal_html(animals_data)
 
         with open(template_file, "r") as file:
             template_content = file.read()
